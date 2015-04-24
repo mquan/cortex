524921dfc9dd3dad3d7b881cfbdd52179b58e072
Michael Fellinger <m.fellinger@gmail.com>
2015-04-24 03:12:33 +0200

    update babelify

--- src/cortex.js
+++ src/cortex.js
@@ -5,6 +5,7 @@ module.exports = (function() {
 
   class Cortex extends DataWrapper {
     constructor(value, callback) {
+      super()
       this.__value = value;
       this.__path = [];
       this.__updates = [];
